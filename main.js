ec53378514f505e0e64217d86c4fe10db0865401
Antonio Pereira <anthonyesany0712@gmail.com>
2023-09-18 20:43:00 -0300

    add main

---
+++ main.js
@@ -0,0 +1,14 @@
+import { renderCatalog } from "./src/cardProduct";
+import { initializeFilters } from "./src/filtersCatalog";
+import {
+  updatePriceCart,
+  initializeCart,
+  renderProductsCart,
+} from "./src/menuCart";
+
+renderCatalog();
+initializeCart();
+renderProductsCart();
+updatePriceCart();
+initializeFilters();
+initializeFilters;
