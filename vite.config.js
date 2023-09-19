f183e2780d452cb36d2953dcc5a8fc0cd67401b5
Antonio Pereira <anthonyesany0712@gmail.com>
2023-09-18 22:10:24 -0300

    deploy settings

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "alexsany-magazine",
+  base: "/alexsany-magazine",
   plugins: [],
   resolve: {
     /*something*/
